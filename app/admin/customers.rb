664b365d9ce747942831c777fa9a5a3d8e422a8f
Cristopher Carandang <ccarandang@academic.rrc.ca>
2017-12-06 04:34:52 +0000

    Fixed form not showing all inputs in ActiveAdmin create customers

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -4,7 +4,15 @@ ActiveAdmin.register Customer do
 
   form do |f|
     f.inputs 'Details' do
+      f.input :first_name, :as => :string
+      f.input :last_name, :as => :string
+      f.input :address, :as => :string
+      f.input :postal_code, :as => :string
       f.input :country, :as => :string
+      f.input :username, :as => :string
+      f.input :email, :as => :string
+      f.input :password, :as => :string
+      f.input :province, :as => :string
     end
     f.actions
   end
